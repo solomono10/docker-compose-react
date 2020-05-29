26edfad7e588726f1c289cf3290559e7ecad7c04
solomono <solomon_wuni@yahoo.co.uk>
2020-05-29 15:06:34 +0100

    make fail

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now!!!
+          Teach React Now!!!
         </a>
       </header>
     </div>
